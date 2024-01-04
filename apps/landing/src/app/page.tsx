26a44988e2374541fafd9919a761a1abe4412355
Ayush Gupta <ayush.keshav2004@gmail.com>
2024-01-05 01:59:10 +0530

    chore: boiler

---
+++ apps/landing/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page(): JSX.Element {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <span className="flex">
+        todo
+      </span>
+    </main>
+  );
+}
